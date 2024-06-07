ef9ec468ab84d52f78a23db9d43871277fdddeb8
Amitesh Mani Tiwari <amiteshmanitiwari1997@gmail.com>
2024-06-08 01:23:06 +0530

    swift ui learning

---
+++ CurrencyCollider/CurrencyColliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyColliderApp.swift
+//  CurrencyCollider
+//
+//  Created by Amitesh Mani Tiwari on 08/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyColliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
