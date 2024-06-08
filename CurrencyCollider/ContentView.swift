1d55a2d32bdf88b1872dd4d016a966d36a0bfc61
Amitesh Mani Tiwari <amiteshmanitiwari1997@gmail.com>
2024-06-08 23:27:46 +0530

    laying out our main screen with stacks(V Stack, Z stack, H stack)

--- CurrencyCollider/ContentView.swift
+++ CurrencyCollider/ContentView.swift
@@ -8,14 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+   @State var showexchangeInfo  = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+            VStack {
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                Text("Currency exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                HStack {
+                    VStack {
+                        HStack {
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                        }
+                        Text("Textfield")
+                    }
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .symbolEffect(.pulse)
+                    VStack {
+                        HStack {
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                        }
+                        Text("Textfield")
+                    }
+                }
+                
+                Spacer()
+                
+                Button {
+                    showexchangeInfo = true
+                } label: {
+                    Image(systemName: "info.circle.fill")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                }
+            }
         }
-        .padding()
     }
 }
 
